d32f9a5786b9895752bc6bde0cf2e87f5a0d0704
Thibaut LERMYTTE <lermytte.t@gmail.com>
2024-07-07 13:59:20 +0000

    Add cof's next handler execution validation

--- src/chainOfResponsability/handler/index.ts
+++ src/chainOfResponsability/handler/index.ts
@@ -1,9 +1,13 @@
 import { Event } from 'ws';
 
 export default abstract class Handler {
-  protected _next: Handler | null = null;
+  private _next: Handler | null = null;
 
   public abstract handle(event: Event): void;
+
+  public nextHandler(event: Event): void {
+    if (this._next) this._next.handle(event);
+  }
 
   public set next(handler: Handler) {
     this._next = handler;
