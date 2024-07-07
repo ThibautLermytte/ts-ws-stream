e0a5ecee83fe1dd436e7da2afa3d0a539db6983d
Thibaut LERMYTTE <lermytte.t@gmail.com>
2024-07-07 12:20:58 +0000

    Add socket in export index

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import Stream from './src/stream';
-import Observer from './src/observer';
-import ChainOfResponsability from './src/chainOfResponsability';
+import Socket from './src/socket';
 import Handler from './src/chainOfResponsability/handler';
 
 export default Stream;
 
-export { Observer, ChainOfResponsability, Handler };
+export { Socket, Handler };
