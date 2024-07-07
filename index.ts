841ff6c75d511c71cddd0c40568618bac219bae9
Thibaut LERMYTTE <lermytte.t@gmail.com>
2024-07-07 11:58:51 +0000

    Remove socket from index exports

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 import Stream from './src/stream';
-import Socket from './src/socket';
 import Observer from './src/observer';
 import ChainOfResponsability from './src/chainOfResponsability';
 import Handler from './src/chainOfResponsability/handler';
 
 export default Stream;
 
-export { Socket, Observer, ChainOfResponsability, Handler };
+export { Observer, ChainOfResponsability, Handler };
