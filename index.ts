659219567bf4aaac1cef021e596294dddba70a18
Thibaut LERMYTTE <lermytte.t@gmail.com>
2024-07-07 11:58:23 +0000

    Add index exports

---
+++ index.ts
@@ -0,0 +1,9 @@
+import Stream from './src/stream';
+import Socket from './src/socket';
+import Observer from './src/observer';
+import ChainOfResponsability from './src/chainOfResponsability';
+import Handler from './src/chainOfResponsability/handler';
+
+export default Stream;
+
+export { Socket, Observer, ChainOfResponsability, Handler };
